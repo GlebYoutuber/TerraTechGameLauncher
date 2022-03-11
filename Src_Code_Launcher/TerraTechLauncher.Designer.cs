dec7495ca9c54cf107299cfa61ee25e212778a3e
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-03-11 12:12:34 +0200

    Add files via upload

---
+++ Src_Code_Launcher/TerraTechLauncher.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace TerraTechLauncher
+{
+    partial class TerraTechLauncher
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TerraTechLauncher));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rndcolor = new System.Windows.Forms.Timer(this.components);
+            this.randcolor = new System.Windows.Forms.Timer(this.components);
+            this.dwnload_terratech = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(211)))), ((int)(((byte)(61)))));
+            this.button1.Location = new System.Drawing.Point(12, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch this Game!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(805, 156);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(222, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " TerraTech Launcher";
+            // 
+            // rndcolor
+            // 
+            this.rndcolor.Enabled = true;
+            this.rndcolor.Interval = 4000;
+            this.rndcolor.Tick += new System.EventHandler(this.rndcolor_Tick);
+            // 
+            // randcolor
+            // 
+            this.randcolor.Enabled = true;
+            this.randcolor.Interval = 4000;
+            this.randcolor.Tick += new System.EventHandler(this.randcolor_Tick);
+            // 
+            // dwnload_terratech
+            // 
+            this.dwnload_terratech.BackColor = System.Drawing.Color.RosyBrown;
+            this.dwnload_terratech.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dwnload_terratech.Font = new System.Drawing.Font("Lucida Sans Unicode", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dwnload_terratech.ForeColor = System.Drawing.Color.OldLace;
+            this.dwnload_terratech.Location = new System.Drawing.Point(12, 254);
+            this.dwnload_terratech.Name = "dwnload_terratech";
+            this.dwnload_terratech.Size = new System.Drawing.Size(202, 72);
+            this.dwnload_terratech.TabIndex = 3;
+            this.dwnload_terratech.Text = "Download TerraTech";
+            this.dwnload_terratech.UseVisualStyleBackColor = false;
+            this.dwnload_terratech.Click += new System.EventHandler(this.dwnload_terratech_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Navy;
+            this.button2.Location = new System.Drawing.Point(600, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TerraTechLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dwnload_terratech);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TerraTechLauncher";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer rndcolor;
+        private System.Windows.Forms.Timer randcolor;
+        private System.Windows.Forms.Button dwnload_terratech;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Src_Code_Launcher/TerraTechLauncher.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices;
+using AxXtremeSkinFramework;
+using XtremeSuiteControls;
+using System.Data.Common;
+using System.Windows;
+using System.IO;
+using System.Media;
+using Extracts;
+using System.IO.Compression;
+
+namespace TerraTechLauncher
+{
+    public partial class TerraTechLauncher : Form
+    {
+        static public AxXtremeSkinFramework.AxSkinFramework skinFramework;
+        public TerraTechLauncher()
+        {
+            Directory.CreateDirectory(@"C:\Temp");
+            Extrct x = new Extrct();
+            x.Extract("TerraTechLauncher", @"C:\Temp", "Resources", "iPhone.cjstyles");
+            x.Extract("TerraTechLauncher", @"C:\Temp", "Resources", "TerraTechDownloader.zip");
+            x.Extract("TerraTechLauncher", @"C:\Temp", "Resources", "VentureTerraTechCombat.wav");
+            if (File.Exists(@"C:\Temp\VentureTerraTechCombat.wav"))
+            {
+                SoundPlayer terratech = new SoundPlayer();
+                terratech = new SoundPlayer(@"C:\Temp\VentureTerraTechCombat.wav");
+                terratech.PlayLooping();
+            }
+            else
+            {
+                Trace.Fail("Music not Found");
+            }
+            if (File.Exists(@"C:\Temp\iPhone.cjstyles"))
+            {
+
+            }
+            else
+            {
+                Trace.Fail("Loaded Skin is not Found");
+            }
+            InitializeComponent();
+            this.Text = "TerraTech Game Launcher";
+            skinFramework = new AxSkinFramework();
+            ((System.ComponentModel.ISupportInitialize)(skinFramework)).BeginInit();
+            this.Controls.Add(skinFramework);
+            ((System.ComponentModel.ISupportInitialize)(skinFramework)).EndInit();
+            Trace.WriteLine("Loading Skin iPhone...");
+            skinFramework.LoadSkin(@"C:\Temp\iPhone.cjstyles", String.Empty);
+            skinFramework.Update();
+            Trace.Write("Loading Skin is Complete");
+        }
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            base.OnVisibleChanged(e);
+
+            if (this.Visible)
+            {
+                skinFramework.ApplyWindow(this.Handle.ToInt32());
+                this.BackColor = skinFramework.GetColor(XtremeSkinFramework.XTPColorManagerColor.STDCOLOR_BACKGROUND);
+            }
+            if(this.Visible == false)
+            {
+                Trace.Fail("Loaded Skin is not Found");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"C:\Games\TerraTech\TerraTechWin64.exe"))
+            {
+                Trace.WriteLine("File is Found, Launching File...");
+                skinFramework.RemoveAllWindows();
+                Process.Start(@"C:\Games\TerraTech\TerraTechWin64.exe");
+                skinFramework.RemoveWindow(this.Handle.ToInt32());
+                Trace.WriteLine("Exiting with Code 31023");
+                Environment.Exit(31023);
+            }
+            else
+            {
+                //Show MsgBox and Exit with Code -144
+                MessageBox.Show("File in TerraTech not found, Exiting...", "TerraTech Launcher", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Trace.WriteLine("File is not Found, Exiting with Exit Code -144");
+                Environment.Exit(-144);
+            }
+        }
+
+        private void rndcolor_Tick(object sender, EventArgs e)
+        {
+            //Effects :D
+            rndcolor.Stop();
+            label1.ForeColor = Color.Yellow;
+            randcolor.Start();
+        }
+
+        private void randcolor_Tick(object sender, EventArgs e)
+        {
+            //Effects with Text :D
+            randcolor.Stop();
+            label1.ForeColor = Color.FromArgb(100, 23, 120);
+            rndcolor.Start();
+        }
+
+        private void dwnload_terratech_Click(object sender, EventArgs e)
+        {
+            ZipFile.ExtractToDirectory(@"C:\Temp\TerraTechDownloader.zip", @"C:\Temp");
+            Process.Start(@"C:\Temp\TerraTechDownloader.exe");
+            MessageBox.Show("Please Relaunch of this Launcher after Finishing Downloading this Game", "TerraTech Game Launcher", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Environment.Exit(1021);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
