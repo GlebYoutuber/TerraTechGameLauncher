f3b80218765554370eeea386b7b142607028294d
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-03-11 11:38:06 +0200

    Add files via upload

---
+++ Src_Code_Downloader/TerraTechDownloader.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace TerraTechDownloader
+{
+    partial class TerraTechDownloader
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TerraTechDownloader));
+            this.label1 = new System.Windows.Forms.Label();
+            this.downloadstatus = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(124, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label";
+            // 
+            // downloadstatus
+            // 
+            this.downloadstatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.downloadstatus.ForeColor = System.Drawing.Color.Maroon;
+            this.downloadstatus.Location = new System.Drawing.Point(71, 346);
+            this.downloadstatus.Name = "downloadstatus";
+            this.downloadstatus.Size = new System.Drawing.Size(441, 30);
+            this.downloadstatus.TabIndex = 1;
+            // 
+            // TerraTechDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(665, 445);
+            this.Controls.Add(this.downloadstatus);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TerraTechDownloader";
+            this.Text = "TerraTech Game Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar downloadstatus;
+    }
+}
+
---
+++ Src_Code_Downloader/TerraTechDownloader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerraTechDownloader
+{
+    public partial class TerraTechDownloader : Form
+    {
+        public TerraTechDownloader()
+        {
+            Directory.CreateDirectory(@"C:\Temp");
+            Directory.CreateDirectory(@"C:\TerraTech");
+            InitializeComponent();
+            this.Show();
+            this.label1.Text = String.Empty;
+            using(WebClient web = new WebClient())
+            {
+                web.DownloadProgressChanged += (s, e) => 
+                {
+                    label1.Text = $"Downloaded: {e.ProgressPercentage}% ({e.BytesReceived / 1024})";
+                    downloadstatus.Value = e.ProgressPercentage; 
+                };
+                web.DownloadFileAsync(new Uri("https://objects.githubusercontent.com/github-production-release-asset-2e65be/468667248/50da8eb2-45ba-4c2a-a1a4-c23d6b7ba247?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220311%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220311T083815Z&X-Amz-Expires=300&X-Amz-Signature=f4effb5b6359fc74dcb02cdaa3529a2f29a57edc84dc3f876be90c8544fe11db&X-Amz-SignedHeaders=host&actor_id=73891760&key_id=0&repo_id=468667248&response-content-disposition=attachment%3B%20filename%3DTerraTechSetup.zip&response-content-type=application%2Foctet-stream"), @"C:\Temp\TerraTechSetup.zip");
+                web.DownloadFileCompleted += TerraTechDown;
+            }
+        }
+        private static void TerraTechDown(object sender, AsyncCompletedEventArgs a)
+        {
+            MessageBox.Show("Downloading File is Completed", "TerraTech Game Downloader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ZipFile.ExtractToDirectory(@"C:\Temp\TerraTechSetup.zip", @"C:\TerraTech");
+            if (File.Exists(@"C:\TerraTech\setup.exe"))
+            {
+                Process.Start(@"C:\TerraTech\setup.exe");
+            }
+            Environment.Exit(412);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for using this Program, Made by GlebYoutuber", "TerraTech Game Downloader", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
