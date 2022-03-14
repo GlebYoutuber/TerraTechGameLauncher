0badb245b56cb28820a92454417261e63910d2e5
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-03-14 15:11:35 +0200

    Update Program.cs

--- Src_Code_Launcher/Program.cs
+++ Src_Code_Launcher/Program.cs
@@ -8,9 +8,6 @@ namespace TerraTechLauncher
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
